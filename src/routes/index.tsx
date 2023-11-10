687a860b24ad12735a7d275b7107bf4c7c1ce4c3
hyevvy <hyeg0@naver.com>
2023-11-10 12:11:20 +0900

    feat: Menu 및 라우팅 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -27,10 +27,46 @@ const router = createBrowserRouter([
     ],
   },
   {
-    path: "/",
+    path: "/dashboard",
     element: <MainLayout />,
     errorElement: <NotFound />,
     children: [{ index: true, path: "dashboard", element: <DashBoard /> }],
+  },
+  {
+    path: "/product",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "add", element: <DashBoard /> },
+      { index: true, path: "list", element: <DashBoard /> },
+    ],
+  },
+  {
+    path: "/cash",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "up", element: <DashBoard /> },
+      { index: true, path: "list", element: <DashBoard /> },
+    ],
+  },
+  {
+    path: "/sulbing",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "payfor", element: <DashBoard /> },
+      { index: true, path: "list", element: <DashBoard /> },
+    ],
+  },
+  {
+    path: "/etc",
+    element: <MainLayout />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: "shorts", element: <DashBoard /> },
+      { index: true, path: "live", element: <DashBoard /> },
+    ],
   },
 ]);
 export default router;
