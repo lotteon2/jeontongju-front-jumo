ea4db0607d29449ca3e585ed70912559bd88fee3
hyevvy <hyeg0@naver.com>
2023-11-10 14:37:21 +0900

    feat: addProduct 목업

---
+++ src/constants/AddProductFieldType.ts
@@ -0,0 +1,11 @@
+export type AddProductFieldType = {
+	productName?: string;
+	desc?: string;
+	alcoholPercent?: number;
+	material?: string;
+	sour?: number;
+	sweet?: number;
+	scent?: number;
+	body?: number;
+	carbonation?: number;
+};
