52888636f5892a1ee6e3093e297de87170c452cb
hyevvy <hyeg0@naver.com>
2023-11-20 10:28:40 +0900

    feat: 주문 내역 목업

--- src/components/OrderList/OrderState.tsx
+++ src/components/OrderList/OrderState.tsx
@@ -1,15 +1,34 @@
 import styled from "@emotion/styled";
+import { Input } from "antd";
 import {
   ORDER_STATE,
   translateOrderState,
 } from "../../constants/OrderStateType";
+import Button from "../common/Button";
 
 const OrderState = ({ state }: { state: keyof typeof ORDER_STATE }) => {
-  return <StyledOrderState>{translateOrderState(state)}</StyledOrderState>;
+  return (
+    <div>
+      {state === ORDER_STATE.ORDER ? (
+        <StyledOrderInput>
+          <Input placeholder="운송장 번호를 입력해주세요." />
+          <Button content="저장" Key="saveOrderNumber" btnType="cancel" />
+        </StyledOrderInput>
+      ) : (
+        <StyledOrderState>{translateOrderState(state)}</StyledOrderState>
+      )}
+    </div>
+  );
 };
 export default OrderState;
 
 const StyledOrderState = styled.div`
-  width: 3rem;
-  border: none;
+  border-radius: 8px;
+  text-align: center;
+  background-color: var(--primary-green);
+  color: white;
+`;
+
+const StyledOrderInput = styled.div`
+  display: flex;
 `;
