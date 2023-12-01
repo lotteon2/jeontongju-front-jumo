720e6c5e45863bb0c03c0cf494ffb3f28f4f2172
hyevvy <hyeg0@naver.com>
2023-12-01 15:23:33 +0900

    test: 원료 띄어쓰기 추가

--- src/constants/ProductType/ConceptType.ts
+++ src/constants/ProductType/ConceptType.ts
@@ -1,21 +1,21 @@
 import { SelectProps } from 'antd';
 
 export const CONCEPT = {
-	CAMPING: '⛺️캠핑',
-	FISHING: '🎣낚시',
-	PARTY: '🪩파티',
-	GATHERING: '👥모임',
+	CAMPING: '⛺️ 캠핑',
+	FISHING: '🎣 낚시',
+	PARTY: '🪩 파티',
+	GATHERING: '👥 모임',
 	YEAR_END: '연말',
-	HIKING: '🧗‍♀️등산',
-	OUTING: '🧺나들이',
-	TRIP: '🚗여행',
-	GIFT: '🎁선물',
-	MEETING_THE_FAMILY: '👨‍👩‍👧‍👧상견례',
-	HOLIDAY: '🎎명절',
-	ROOPTOP: '🌕루프탑',
-	HEALING: '🏥힐링',
-	EMOTION: '❤️감성',
-	HANGOVER_REMEDY: '🤮숙취퇴치',
+	HIKING: '🧗‍♀️ 등산',
+	OUTING: '🧺 나들이',
+	TRIP: '🚗 여행',
+	GIFT: '🎁 선물',
+	MEETING_THE_FAMILY: '👨‍👩‍👧‍👧 상견례',
+	HOLIDAY: '🎎 명절',
+	ROOPTOP: '🌕 루프탑',
+	HEALING: '🏥 힐링',
+	EMOTION: '❤️ 감성',
+	HANGOVER_REMEDY: '🤮 숙취퇴치',
 };
 
 export const ConceptOptions: SelectProps['options'] = [];
