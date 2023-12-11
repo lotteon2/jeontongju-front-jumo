2dce79319f8f127ecfbe67b426adef658a16f377
hyevvy <hyeg0@naver.com>
2023-12-11 17:59:45 +0900

    test: 중간 커밋

--- src/pages/Login/Login.hooks.ts
+++ src/pages/Login/Login.hooks.ts
@@ -38,13 +38,6 @@ export const useLogin = () => {
 				Toast(true, '로그인되었어요');
 				setIsLogin(true);
 				localStorage.setItem('accessToken', data.data.accessToken);
-				if (myInfo) {
-					if (myInfo.data) {
-						setApprovalState(myInfo.data.approvalState);
-						setStoreImageUrl(myInfo.data.storeImageUrl);
-						setStoreName(myInfo.data.storeName);
-					}
-				}
 				navigate('/');
 			}
 		} catch (err) {
