df03b7f0f255127f26dd078aa9e6538eb12a3545
hyevvy <hyeg0@naver.com>
2023-12-12 12:08:31 +0900

    test: 중간 커밋

--- src/components/common/ImageUploader.tsx
+++ src/components/common/ImageUploader.tsx
@@ -30,33 +30,32 @@ const ImageUploader: React.FC<ImageUploaderInterface> = ({ imageUrl, setImageUrl
 			const data = await storageApi.uploadS3(event.target.files[0].name);
 			if (data.code === 200) {
 				console.log(event.target.files[0].type);
-				// fetch(data.data.presignedUrl, {
-				// 	method: 'PUT',
-				// 	headers: {
-				// 		Accept: 'application/json',
-				// 		Authorization: undefined,
-				// 		'Content-Type': event.target.files[0].type,
-				// 	},
-				// 	body: {
-				// 		image: event.target.files[0],
-				// 	},
-				// })
-				// 	.then((res) => {
-				// 		return res.text();
-				// 	})
-				// 	.then((value) => {
-				// 		setImageUrl(value);
-				// 	});
-
-				axios
-					.put(data.data.presignedUrl, formData, {
-						headers: {
-							Accept: 'image/png',
-							Authorization: undefined,
-							'Content-Type': 'image/png',
-						},
+				fetch(data.data.presignedUrl, {
+					method: 'PUT',
+					headers: {
+						Accept: 'image/png',
+						Authorization: undefined,
+						'Content-Type': event.target.files[0].type,
+					},
+					body: event.target.files[0],
+				})
+					.then((res) => {
+						return res.text();
 					})
-					.then((res) => console.log(res));
+					.then((value) => {
+						console.log(value);
+						// setImageUrl(value);
+					});
+
+				// axios
+				// 	.put(data.data.presignedUrl, formData, {
+				// 		headers: {
+				// 			Accept: 'image/png',
+				// 			Authorization: undefined,
+				// 			'Content-Type': 'image/png',
+				// 		},
+				// 	})
+				// 	.then((res) => console.log(res));
 				console.log(data.data.presignedUrl);
 			}
 		} catch (error) {
