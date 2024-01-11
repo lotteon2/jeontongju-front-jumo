0adec02b07cc9b532736aa4712df8492913f2d42
hyevvy <hyeg0@naver.com>
2024-01-11 16:19:58 +0900

    test: 중간 커밋

--- src/pages/Etc/LiveRegister/LiveRegister.tsx
+++ src/pages/Etc/LiveRegister/LiveRegister.tsx
@@ -96,7 +96,7 @@ const LiveRegister = () => {
 					isfull
 					handleClick={onSubmit}
 					htmlType="submit"
-					disabled={disabled}
+					// disabled={disabled}
 				/>
 			</Form>
 		</StyledLiveRegisterPage>
