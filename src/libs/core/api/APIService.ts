6b5f4753d02d51aead5ebf882cdd7d5c0f5f80dc
hyevvy <hyeg0@naver.com>
2024-01-11 16:09:36 +0900

    feat: origin 설정

--- src/libs/core/api/APIService.ts
+++ src/libs/core/api/APIService.ts
@@ -16,7 +16,7 @@ class APIService {
 
 	headers: Headers = {
 		'Content-Type': 'application/json',
-		'Access-Control-Allow-Origin': 'https://jeontongju-dev.shop',
+		'Access-Control-Allow-Origin': 'https://api.jeontongju.shop',
 		Authorization: localStorage.getItem('accessToken') || '',
 		'Access-Control-Allow-Credentials': 'true',
 	};
